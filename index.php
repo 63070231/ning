0c259e4956d71088ba33f1f7587b471b2c56cb5d
63070231 <73694998+63070231@users.noreply.github.com>
2020-10-30 17:26:40 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Hello world</title>
+    </head>
+    
+    <body>
+        <h1>My Website</h1>
+        <img src="cat.jpg">
+        <br>
+        <a href="https://www.readawrite.com/">my favorite</a>
+        
+        <ul>
+            <li>My name is Narisara.</li>
+            <li>My lastname is Rinkaew.</li>
+            <li>You can called me Ning.</li>
+        </ul>
+        <h3>เรื่องตลกๆๆๆๆๆๆๆๆๆๆๆ</h3>
+        <img src="joke.jpg">
+        <br>
+        <p>A:ไอ้ เสื้อ กางเกง ชุดชั้นใน กางเกงใน ถุงเท้า <p>
+        <p>ฺB:หื้อ??????<p>
+        <p>A:ด่าเป็นชุดดด</p>
+        <p>B:ไอ้เสื้อไม่รีด</p>
+        <p>A:...อะไรอ่ะ</p>
+        <p>B:ด่ายับ!!!</p>
+    </body>
+</html>
