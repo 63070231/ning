184ecc79fb562d6d93becfc4b5c11fbf15f0887c
63070231 <73694998+63070231@users.noreply.github.com>
2020-11-27 17:58:18 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -17,26 +17,11 @@ $sql = "INSERT INTO guesbook (name, height, weight,bmi) VALUES ('$name', '$heigh
 
 
 if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
+     echo "<script>alert('New record created successfully')</script>";
+      echo "<script>window.location='show.php'</script>";
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
   
 mysqli_close($conn);
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<title>Insert New Record</title>
-<link rel="stylesheet" href="css/style.css" />
-</head>
-<body>
-|   <a href="https://wab-narisara.azurewebsites.net/show.php" class="btn btn-primary btn-sm active" role="button" aria-pressed="true">Show Table</a>
-</body>
-</html>
